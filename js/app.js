9562ced2b4b9108bba0595cf4c0179b34bf00109
Jomar Mendoza <Omay@Macintosh-2.local>
2019-03-20 16:10:59 -0700

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -9,10 +9,24 @@
 // }
 
 
-for(let i = 0; i <= 20; i++){
-  console.log("Love me, pet me! HSSSSSS!")
-  const cat = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
-  if(i % 2 === 0){
-    console.log(cat[Math.floor(Math.random() * 3)])
+// for(let i = 0; i <= 20; i++){
+//   console.log("Love me, pet me! HSSSSSS!")
+//   const cat = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+//   if(i % 2 === 0){
+//     console.log(cat[Math.floor(Math.random() * 3)])
+//   }
+// }
+
+
+for(let i = 0; i <= 15; i++){ // 15 == 100
+  console.log(i)
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log("fizzbuzz")
+  }
+  else if(i % 3 === 0){
+    console.log("fizz")
+  }
+  else if(i % 5 === 0){
+    console.log("buzz")
   }
 }
