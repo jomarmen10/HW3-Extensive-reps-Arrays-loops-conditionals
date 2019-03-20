a0f7101ff5d733e620c130b8d3afdf9d8a801b2f
Jomar Mendoza <Omay@Macintosh-2.local>
2019-03-20 16:06:18 -0700

    Commit 2 - Get Even answered

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-for(let i = 0; i <=20; i++){
-  console.log(i)
+// for(let i = 0; i <=20; i++){
+//   console.log(i)
+// }
+
+for(let i = 0; i <= 20; i++){ // 20 == 200
+  if(i % 2 === 0){
+    console.log(i)
+  }
 }
