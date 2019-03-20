816e9631ee935ed28480bd0d5369f7d1d573c243
Jomar Mendoza <Omay@Macintosh-2.local>
2019-03-20 16:56:25 -0700

    Commit 5 - Getting to Know You answered

--- js/app.js
+++ js/app.js
@@ -18,15 +18,46 @@
 // }
 
 
-for(let i = 0; i <= 15; i++){ // 15 == 100
-  console.log(i)
-  if(i % 3 === 0 && i % 5 === 0){
-    console.log("fizzbuzz")
-  }
-  else if(i % 3 === 0){
-    console.log("fizz")
-  }
-  else if(i % 5 === 0){
-    console.log("buzz")
+// for(let i = 0; i <= 15; i++){ // 15 == 100
+//   console.log(i)
+//   if(i % 3 === 0 && i % 5 === 0){
+//     console.log("fizzbuzz")
+//   }
+//   else if(i % 3 === 0){
+//     console.log("fizz")
+//   }
+//   else if(i % 5 === 0){
+//     console.log("buzz")
+//   }
+// }
+
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+
+kenny[0] = "Gameboy";
+//console.log(kenny)
+
+for (let i = 0; i < jimClark.length; i++){
+  if(!isNaN(jimClark[i])){
+    jimClark[i]++;
   }
 }
+
+//console.log(jimClark)
+
+ryan[2] = "Gotham City";
+//console.log(ryan)
+
+reuben.pop();
+reuben.push("Chicago")
+//console.log(reuben)
+
+jimHaff.splice(2, 1, "city","city1","city3")
+//console.log(jimHaff)
+jimHaff.splice(2, 1)
+//console.log(jimHaff)
